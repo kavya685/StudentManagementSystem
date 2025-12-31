9d0060bcf81dfdb765f147b7c3d8867575eeed33
kavya685 <kavyapopat95@gmail.com>
2025-12-31 19:04:31 +0530

    added security to DBConnection.java file

--- src/main/java/com/studentapp/util/DBConnection.java
+++ src/main/java/com/studentapp/util/DBConnection.java
@@ -6,7 +6,7 @@ public class DBConnection {
 
   private static final String URL = "jdbc:mysql://localhost:3306/student";
   private static final String USER = "root";
-  private static final String PASSWORD = "radhe";
+  //replace password with your local DB password
 
   private static Connection con;
 
